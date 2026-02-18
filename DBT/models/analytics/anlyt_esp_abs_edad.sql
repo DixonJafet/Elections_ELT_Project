53ae5624a8f05f1ac1fd25d4462e9018295b34c1
Jafet <106804873+DixonJafet@users.noreply.github.com>
2026-02-18 06:13:39 -0600

    Update anlyt_esp_abs_edad.sql

--- DBT/models/analytics/anlyt_esp_abs_edad.sql
+++ DBT/models/analytics/anlyt_esp_abs_edad.sql
@@ -2,4 +2,4 @@ WITH get_src_abs_edad AS (
     SELECT * FROM {{ ref('src_abs_edad') }}
 )
 
-SELECT * FROM get_src_abs_edad /* pendiente corregir el primer rango de edad, en esta solicitud*/
+SELECT * FROM get_src_abs_edad
